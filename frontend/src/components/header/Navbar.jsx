53502114224f200cf4d61df809b30936682c2e84
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-08 23:45:50 +0500

    Built a navbar without profile link

---
+++ frontend/src/components/header/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <>
+      <nav className="bg-blue-900 w-full ">
+        <img src="/logo.png" alt="Logo" className="lg:w-1/6 w-1/4" />
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
