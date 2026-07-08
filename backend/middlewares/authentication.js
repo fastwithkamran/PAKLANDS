9e05d8fac6cb56e9701467e7cab2931a6d85be2b
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-08 22:59:35 +0500

    Add meaningfull status codes and JSON errors

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -3,12 +3,13 @@ const { validateToken } = require("../services/auth");
 function checkForAuthenticationCookie(cookieName) {
   return (req, res, next) => {
     const tokenCookievalue = req.cookies[cookieName];
-    if (!tokenCookievalue) return next();
+    if (!tokenCookievalue) return res.status(404).json({ msg: "User must be Login" });
 
     try {
       const userPayload = validateToken(tokenCookievalue);
       req.user = userPayload;
     } catch (error) {
+      return res.status(404).json({ msg: "Please Login Again" });
       console.log(error);
     }
 
