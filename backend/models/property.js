b9473c6dfb979455b475b3e7a417a00d91eee3b5
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-08 19:01:48 +0500

    Implement Schema for Property

---
+++ backend/models/property.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const propertySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    propertyImages: {
+      type: String,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Property = model("properties", propertySchema);
+
+module.exports = Property;
