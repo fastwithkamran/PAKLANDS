71f2b76494135cc867233ce8a2c23a7ca52bf772
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-08 17:49:43 +0500

    Implement Routes for auth pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import App from "./App.jsx";
+
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router";
+
+import { Signup, Login } from "./components";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/user/signup" element={<Signup />} />
+      <Route path="/user/login" element={<Login />} />
+    </Route>,
+  ),
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
