825aa5a7b905373cbe81abdbdda1a7f6ba62f42d
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-08 18:02:51 +0500

    remove console logs

--- backend/controllers/signup.js
+++ backend/controllers/signup.js
@@ -5,9 +5,6 @@ const handleUserSignUp = async (req, res) => {
     const { fullName, email, password, cnic, phone, role } = req.body;
 
     const avatorUrl = req.file ? req.file.secure_url : "https://www.google.com/imgres?q=avatorurl&imgurl=https%3A%2F%2Fimg.magnific.com%2Fpremium-photo%2Fhuman-resources-manager-digital-avatar-generative-ai_934475-9192.jpg%3Fsemt%3Dais_hybrid%26w%3D740%26q%3D80&imgrefurl=https%3A%2F%2Fwww.magnific.com%2Ffree-photos-vectors%2Favatar&docid=ZoMniqxlLaLBzM&tbnid=1W8MKrDI-xCTnM&vet=12ahUKEwitru-F0sKVAxW4VqQEHfcLE6sQnPAOegQILxAA..i&w=740&h=740&hcb=2&ved=2ahUKEwitru-F0sKVAxW4VqQEHfcLE6sQnPAOegQILxAA";
-
-    console.log("avatorUrl", avatorUrl);
-    console.log("req.file", req.file);
     
     await User.create({
       fullName,
