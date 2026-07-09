3e38bfc5fc47217fe304a4319d0689ba411ed640
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-09 13:03:26 +0500

    Use Outlet and HTML Semantic tags and write nested route rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import { Navbar } from "./components";
+import { Footer } from "./components";
 import { Outlet } from "react-router";
 
 function App() {
   return (
     <>
-      <Outlet />
+      <div className="flex flex-col min-h-screen w-full overflow-x-hidden">
+        <nav>
+          <Navbar />
+        </nav>
+        <main className="flex flex-col grow justify-center items-center bg-gray-200 p-4">
+          <Outlet />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
     </>
   );
 }
