3933e92f5a8c23abfb9dd873b3ebcdd9afcf2f23
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-09 10:50:52 +0500

    Create hero section for buyer page

---
+++ frontend/src/components/hero/BuyerHome.jsx
@@ -0,0 +1,15 @@
+function BuyerHome() {
+  return (
+    <>
+    <main>
+    <div className="grid md:grid-cols-3 grid-cols-2 w-full mt-2">
+    <img src="/home_1.png" alt="home_1" className="w-full object-cover h-full" />
+    <img src="/home_2.png" alt="home_2" className="w-full object-cover h-full" />
+    <img src="/home_3.png" alt="home_3" className="w-full object-cover h-full hidden md:block" />
+    </div>
+    </main>
+    </>
+  )
+}
+
+export default BuyerHome
