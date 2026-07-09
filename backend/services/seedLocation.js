95cc28e8f66a10ff39e8839e6e01b23cc1667df7
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-09 22:56:13 +0500

    Fixed: the geoJson file path

--- backend/services/seedLocation.js
+++ backend/services/seedLocation.js
@@ -1,22 +1,24 @@
 const fs = require("fs");
+const path = require("path");
 const Location = require("../models/location");
 
 const handleSeedLocations = async (req, res) => {
   try {
-    const rawData = fs.readFileSync("./location.geojson");
+    const filePath = path.join(__dirname, "location.geojson");
+    const rawData = fs.readFileSync(filePath);
     const result = JSON.parse(rawData);
 
     const Records = result.features.map((place) => ({
       country: "Pakistan",
-      province: features.properties.adm1_name,
-      city: features.properties.adm2_name,
-      area: features.properties.adm3_name,
+      province: place.properties.adm1_name,
+      city: place.properties.adm2_name,
+      area: place.properties.adm3_name,
     }));
 
     const uniqueMap = new Map();
 
     Records.forEach((element) => {
-      const key = `${element.city}-${element.item}`;
+      const key = `${element.city}-${element.area}`;
       if (!uniqueMap.has(key)) {
         uniqueMap.set(key, element);
       }
