251a1e6a4fbe83f1f1b253fd4818f474a92a07e3
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-09 20:28:05 +0500

    Created routes for get request for locations from Frontend

---
+++ backend/routers/location.js
@@ -0,0 +1,54 @@
+const { Router } = require("express");
+const router = Router();
+
+const Location = require("../models/location");
+
+router.get("/get-provinces", async (req, res) => {
+  try {
+    const provincesList = await Location.find({ type: "province" });
+
+    if (!provincesList || provincesList.length === 0)
+      return res.status(500).json({ msg: "Cannot Get Provinces From Server" });
+
+    res.status(200).json(provincesList);
+  } catch (error) {
+    console.log("Error", error);
+    return res.status(500).json({ msg: "Cannot Get Provinces From Server" });
+  }
+});
+
+router.get("/get-cities", async (req, res) => {
+  try {
+    const provinceName = req.query;
+
+    if (!provinceName || provinceName.length === 0)
+      return res.status(404).json({ msg: "Province not found" });
+
+    const cities = await Location.find({ province: provinceName }).distinct(
+      "city",
+    );
+
+    res.status(200).json(cities);
+  } catch (error) {
+    console.log("Error", error);
+    return res.status(500).json({ msg: "Cannot Get Provinces From Server" });
+  }
+});
+
+router.get("/get-areas", async (req, res) => {
+  try {
+    const cityName = req.query;
+
+    if (!cityName || cityName.length === 0)
+      return res.status(404).json({ msg: "City Not Found" });
+
+    const areas = await Location.find({ city: cityName }).distinct("area");
+
+    return res.status(200).json(areas);
+  } catch (error) {
+    console.log("Error", error);
+    return res.status(500).json({ msg: "Cannot Get Provinces From Server" });
+  }
+});
+
+module.exports = router;
