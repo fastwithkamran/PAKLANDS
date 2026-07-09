88c7392ec144b1980b7901fd96daa52220de9bfd
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-09 13:33:14 +0500

    Remove roles: user can create posts based on authentication

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,24 @@
+function Home() {
+  return (
+    <>
+      <div className="grid md:grid-cols-3 grid-cols-2 w-full">
+        <img
+          src="/home_1.png"
+          alt="home_1"
+          className="w-full object-cover h-full"
+        />
+        <img
+          src="/home_2.png"
+          alt="home_2"
+          className="w-full object-cover h-full"
+        />
+        <img
+          src="/home_3.png"
+          alt="home_3"
+          className="w-full object-cover h-full hidden md:block"
+        />
+      </div>
+    </>
+  );
+}
+export default Home;
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,24 +3,19 @@ import Login from "./Login";
 
 import Navbar from "./header/Navbar";
 
-import Home from './Home.jsx'
-
-import SellerHome from "./hero/SellerHome.jsx";
-import BuyerHome from "./hero/BuyerHome.jsx";
+import Home from "./Home.jsx";
 
 import CreateProperty from "./property/CreateProperty";
 import PropertyPage from "./property/PropertyPage";
 import SellerPosts from "./property/SellerPosts";
 
-import Footer from "./footer/Footer.jsx"
+import Footer from "./footer/Footer.jsx";
 
 export {
   Signup,
   Login,
   Navbar,
   Home,
-  SellerHome,
-  BuyerHome,
   CreateProperty,
   PropertyPage,
   SellerPosts,
