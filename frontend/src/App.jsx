4760699d19ed998d1e4a78ef50c95ef364183a9f
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-09 13:33:50 +0500

    fixed the UI by using ternary operator to explicitly remove grow from Home Page only

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
 import { Navbar } from "./components";
 import { Footer } from "./components";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 
 function App() {
+  const location = useLocation();
+  const atHome = location.pathname === "/";
+
   return (
     <>
       <div className="flex flex-col min-h-screen w-full overflow-x-hidden">
         <nav>
           <Navbar />
         </nav>
-        <main className="flex flex-col grow justify-center items-center bg-gray-200 p-4">
+        <main
+          className={
+            atHome
+              ? "flex flex-col justify-center items-center bg-gray-200 p-4"
+              : "flex flex-col grow justify-center items-center bg-gray-200 p-4"
+          }
+        >
           <Outlet />
         </main>
         <footer>
