5cf6c5778055cd453bd997b6a901cdf62f2d6d0b
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-10 19:14:32 +0500

    Created the Create Post button, that validates the user and navigates

--- frontend/src/components/header/Navbar.jsx
+++ frontend/src/components/header/Navbar.jsx
@@ -1,8 +1,44 @@
+import { useLocation, useNavigate } from "react-router";
+
 function Navbar() {
+  const location = useLocation();
+  const atHome = location.pathname === "/";
+  const navigate = useNavigate();
+
+  const handleCreateAd = async () => {
+    try {
+      const response = await fetch(import.meta.env.VITE_AUTH_VERIFICATION_API, {
+        credentials: "include",
+        method: "GET",
+      });
+      console.log(response);
+      const result = await response.json();
+
+      if (response.ok) {
+        navigate("/user/create-property");
+      } else {
+        alert(`Error: ${result.msg}`);
+        navigate("/auth/login");
+      }
+    } catch (error) {
+      console.log("Error", error);
+      alert("Error while Fetching API from Frontend");
+    }
+  };
+
   return (
     <>
-      <nav className="bg-blue-900 w-full ">
+      <nav className="bg-blue-900 flex flex-row justify-between">
         <img src="/logo.png" alt="Logo" className="lg:w-1/6 w-1/4" />
+
+        {atHome && (
+          <button
+            className="border-2 border-amber-50 rounded-lg p-2 m-3 flex items-center bg-red-500 text-amber-50 cursor-pointer hover:bg-gray-500"
+            onClick={handleCreateAd}
+          >
+            Create
+          </button>
+        )}
       </nav>
     </>
   );
