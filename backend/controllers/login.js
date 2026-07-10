ce92f6722c1ea9c9a28ed94423b65d417dd551e3
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-10 17:24:33 +0500

    Added security protocols

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,13 +1,23 @@
 const User = require("../models/user");
+require("dotenv").config();
 
 const handleUserLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
 
     const token = await User.matchPasswordandGenerateToken(email, password);
-    return res.cookie("token", token).json({msg: "Login success"});
+    return res
+      .status(201)
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 90 * 24 * 60 * 60 * 1000,
+      })
+      .json({ msg: "Login Success" });
   } catch {
-    return res.status(404).json({msg: "Incorrect email or password"});
+    console.log("Error in Login", error);
+    return res.status(404).json({ msg: "Incorrect email or password" });
   }
 };
 
--- backend/controllers/signup.js
+++ backend/controllers/signup.js
@@ -1,11 +1,14 @@
 const User = require("../models/user");
+require("dotenv").config();
 
 const handleUserSignUp = async (req, res) => {
   try {
-    const { fullName, email, password, cnic, phone, role } = req.body;
+    const { fullName, email, password, phone } = req.body;
 
-    const avatorUrl = req.file ? req.file.secure_url : "https://www.google.com/imgres?q=avatorurl&imgurl=https%3A%2F%2Fimg.magnific.com%2Fpremium-photo%2Fhuman-resources-manager-digital-avatar-generative-ai_934475-9192.jpg%3Fsemt%3Dais_hybrid%26w%3D740%26q%3D80&imgrefurl=https%3A%2F%2Fwww.magnific.com%2Ffree-photos-vectors%2Favatar&docid=ZoMniqxlLaLBzM&tbnid=1W8MKrDI-xCTnM&vet=12ahUKEwitru-F0sKVAxW4VqQEHfcLE6sQnPAOegQILxAA..i&w=740&h=740&hcb=2&ved=2ahUKEwitru-F0sKVAxW4VqQEHfcLE6sQnPAOegQILxAA";
-    
+    const avatorUrl = req.file
+      ? req.file.secure_url
+      : "https://www.google.com/imgres?q=avatorurl&imgurl=https%3A%2F%2Fimg.magnific.com%2Fpremium-photo%2Fhuman-resources-manager-digital-avatar-generative-ai_934475-9192.jpg%3Fsemt%3Dais_hybrid%26w%3D740%26q%3D80&imgrefurl=https%3A%2F%2Fwww.magnific.com%2Ffree-photos-vectors%2Favatar&docid=ZoMniqxlLaLBzM&tbnid=1W8MKrDI-xCTnM&vet=12ahUKEwitru-F0sKVAxW4VqQEHfcLE6sQnPAOegQILxAA..i&w=740&h=740&hcb=2&ved=2ahUKEwitru-F0sKVAxW4VqQEHfcLE6sQnPAOegQILxAA";
+
     await User.create({
       fullName,
       email,
@@ -14,10 +17,22 @@ const handleUserSignUp = async (req, res) => {
       avator: avatorUrl,
     });
 
-    return res.status(201).json({msg: "Account Created in Server"});
+    const token = await User.matchPasswordandGenerateToken(email, password);
+
+    return res
+      .status(201)
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 90 * 24 * 60 * 60 * 1000,
+      })
+      .json({ msg: "Account Created" });
   } catch (error) {
-    console.log(error)
-    return res.status(500).json({ msg: "Upload failed in Server" , error: error.message});
+    console.log("Error in SignUp", error);
+    return res
+      .status(500)
+      .json({ msg: "Account Not Created", error: error.message });
   }
 };
 
