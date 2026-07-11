8901357f003de295fdd1dea3a56f16cc8cd6691b
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-11 15:00:17 +0500

    Fixed change the set method to get the seller name from result.createdBy.fullName and other details as well.

--- frontend/src/components/property/PropertyPage.jsx
+++ frontend/src/components/property/PropertyPage.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useParams, Link } from "react-router";
 
 function PropertyPage() {
   const [title, setTitle] = useState("");
-  const [description, setDescription] = useState({});
+  const [descriptions, setDescription] = useState([]);
   const [province, setProvince] = useState("");
   const [city, setCity] = useState("");
   const [area, setArea] = useState("");
   const [street, setStreet] = useState("");
-  const [images, setImages] = useState({});
+  const [images, setImages] = useState([]);
   const [isallowWhatsApp, setAllowWhatsApp] = useState(false);
   const [isallowEmail, setAllowEmail] = useState(false);
   const [sellerName, setSellerName] = useState("undefined");
@@ -20,20 +20,23 @@ function PropertyPage() {
   const message = `Hey, ${sellerName}\n\nCustomer is Interest for your property ${title} through PakLands... Reply them as Soon`;
   const encodedMessage = encodeURIComponent(message);
 
-
-  const id = useParams();
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`import.meta.env.VITE_PROPERTY_INFO_API/${id}`, {
-          method: "GET",
-        });
+        const response = await fetch(
+          `${import.meta.env.VITE_PROPERTY_INFO_API}/${id}`,
+          {
+            method: "GET",
+          },
+        );
         const result = await response.json();
-
+        console.log(result);
         if (response.ok) {
           setTitle(result.title);
-          setDescription(result.description);
+          const descriptionArray = JSON.parse(result.description);
+          setDescription(descriptionArray);
           setProvince(result.province);
           setCity(result.city);
           setArea(result.area);
@@ -41,9 +44,9 @@ function PropertyPage() {
           setImages(result.propertyImages);
           setAllowWhatsApp(result.allowWhatsApp);
           setAllowEmail(result.allowEmail);
-          setSellerName(result.fullName);
-          setSellerEmail(result.email);
-          setSellerPhoneNumber(result.phone);
+          setSellerName(result?.createdBy?.fullName || "Unknown Seller");
+          setSellerEmail(result?.createdBy?.email);
+          setSellerPhoneNumber(result?.createdBy?.phone);
         } else {
           alert(`Error: ${result.msg}`);
         }
@@ -53,28 +56,42 @@ function PropertyPage() {
       }
     };
     fetchData();
-  }, []);
+  }, [id]);
 
   return (
     <>
+      <div className="mr-auto">
+        <Link
+          to="/"
+          className="md:text-2xl bg-amber-50 rounded-2xl border-2 p-2 text-pink-800"
+        >
+          Back
+        </Link>
+      </div>
+
       <h1 className="font-bold text-5xl"> {title} </h1>
       <p className="text-sm">
-        {street}, {area}, {city}, {province}, Pakistan
+        📍 {street}, {area}, {city}, {province}, Pakistan
       </p>
 
-      <div className="grid md:grid-cols-3 grid-cols-2 w-full">
-        {images.forEach((element) => {
-          <img src={element} alt="Image" />;
-        })}
+      <div className="grid md:grid-cols-3 grid-cols-2 w-full mt-4">
+        {images?.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt="Image"
+            className="object-cover w-full h-full p-2 border-2 border-amber-50 bg-blue-400"
+          />
+        ))}
       </div>
 
       <div className="grid grid-cols-2 items-start gap-6 w-full">
         <div className="w-full">
-          <h3 className="font-bold text-4xl text-blue-800">Features</h3>
-          <ol className="list-decimal mx-4 ">
-            {description.forEach((element) => {
-              <li>{element}</li>;
-            })}
+          <h3 className="font-bold text-4xl text-blue-800 mt-4">Features</h3>
+          <ol className="list-decimal mx-4 mt-2">
+            {descriptions?.map((element, index) => (
+              <li key={index}>{element}</li>
+            ))}
           </ol>
         </div>
 
@@ -85,26 +102,37 @@ function PropertyPage() {
               alt="Profile Image"
               className="h-20  mx-auto"
             />
-            <div className="mt-2 font-bold"> Created By: ${sellerName}</div>
-            <h3 className="mt-2 font-bold">Interested in this property?</h3>
-            {isallowEmail} ?
-            <a
-              href={`mailto:${sellerEmail}?subject=${subject}&body=${encodedBody}`}
-              className="bg-blue-500 text-white font-bold mt-3 mx-auto px-2 rounded-md hover:bg-gray-600 "
+            <div className="mt-2 font-bold"> Created By: {sellerName} </div>
+            <h3 className="mt-2 font-bold mb-2">
+              Interested in this property?
+            </h3>
+            <div
+              className="flex
+             flex-col"
             >
-              Email
-            </a>
-            : <a></a>
-            {isallowWhatsApp} ?
-            <a
-              href={`https://wa.me${sellerPhoneNumber}?text=${encodedMessage}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-green-500 text-white font-bold mt-3 mx-auto px-2 rounded-md hover:bg-gray-600 "
-            >
-              WhatsApp
-            </a>
-            : <a></a>
+              {isallowEmail ? (
+                <a
+                  href={`mailto:${sellerEmail}?subject=${subject}&body=${encodedBody}`}
+                  className="bg-blue-500 text-white font-bold mx-auto p-1 rounded-md hover:bg-gray-600 "
+                >
+                  Email
+                </a>
+              ) : (
+                <a></a>
+              )}
+              {isallowWhatsApp ? (
+                <a
+                  href={`https://wa.me${sellerPhoneNumber}?text=${encodedMessage}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-green-500 text-white font-bold mt-2 mx-auto p-1 rounded-md hover:bg-gray-600 "
+                >
+                  WhatsApp
+                </a>
+              ) : (
+                <a></a>
+              )}
+            </div>
           </div>
         </div>
       </div>
