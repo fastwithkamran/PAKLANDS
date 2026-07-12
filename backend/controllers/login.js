5b1b9321a3e13dc07fd495ec8a04aacdfd58cbc2
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-12 14:42:02 +0500

    Assign a path while generating cookie

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -9,15 +9,16 @@ const handleUserLogin = async (req, res) => {
     return res
       .status(201)
       .cookie("token", token, {
+        path: "/",
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "lax",
         maxAge: 90 * 24 * 60 * 60 * 1000,
       })
       .json({ msg: "Login Success" });
-  } catch(error) {
-    console.log("Error in Login", error);
-    return res.status(404).json({ msg: "Incorrect email or password" });
+  } catch (error) {
+    console.error("Error in Login Page ", error);
+    return res.status(500).json({ msg: "Cannot Login" });
   }
 };
 
