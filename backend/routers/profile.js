4d993926f93b175f017e7be2de2513e76cd6f556
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-13 12:53:04 +0500

    Added  and Use multer and Cloudinary configs to takes formData from frontend

--- backend/routers/profile.js
+++ backend/routers/profile.js
@@ -1,6 +1,32 @@
 const { Router } = require("express");
 const router = Router();
 
+require("dotenv").config();
+
+const multer = require("multer");
+const cloudinaryModule = require("cloudinary");
+const cloudinary = cloudinaryModule.v2;
+const CloudinaryStorage = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinaryModule,
+  params: {
+    folder: "real-estate/avatars",
+    allowed_formats: ["jpg", "jpeg", "png"],
+  },
+});
+
+
+const upload = multer({ storage: storage });
+
 const handleUpdateProfile = require("../controllers/updateProfile");
 
-router.patch("/profile-update/:id", handleUpdateProfile);
+router.patch("/profile-update/:id", upload.single("avator"), handleUpdateProfile);
+
+module.exports = router;
