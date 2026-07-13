c80dcd94fec668d1ce5a0537c3db4ad4c6c70c55
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-13 12:57:28 +0500

    Create a footer section, only a line for testing layout purpose

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <div className="bg-black w-full">
+      <div className="grid grid-cols-3">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="text-sm text-white text-center">
+        Developed By Kamran 2026
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
