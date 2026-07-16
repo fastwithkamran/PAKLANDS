da31475296259ad3c1eeb43e7765d20ca7c492fc
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-16 23:00:33 +0500

    Added Update Profile and Delete Account status in userSlice Redux

--- frontend/src/redux/user/userSlice.js
+++ frontend/src/redux/user/userSlice.js
@@ -21,8 +21,30 @@ const userSlice = createSlice({
       toast.error(action.payload);
       state.loading = false;
     },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+    },
+    updateUserFailure: (state, action) => {
+      toast.error(action.payload);
+      state.loading = false;
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+    },
+    deleteUserFailure: (state, action) => {
+      toast.error(action.payload);
+      state.loading = false;
+    },
   },
 });
 
-export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
+export const { loginStart, loginSuccess, loginFailure, updateUserStart, updateUserFailure, updateUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess } = userSlice.actions;
 export default userSlice.reducer;
