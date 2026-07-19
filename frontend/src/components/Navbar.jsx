d61322fe42764209149d63f2c0acdc98b0e5cb3d
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-19 14:05:44 +0500

    Implement search functionality, and update it when location.search changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,60 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router";
-import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-hot-toast";
+import { useEffect, useState } from "react";
+import {
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+} from "../redux/user/userSlice.js";
 
 function Navbar() {
   const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermfromURL = urlParams.get("searchTerm");
+
+    if (searchTermfromURL) {
+      setSearchTerm(searchTermfromURL);
+    }
+  }, [location.search]);
+
+  const handleLogOut = async () => {
+    try {
+      dispatch(deleteUserStart());
+
+      const response = await fetch("api/user/logout", {
+        method: "GET",
+        credentials: "include",
+      });
+
+      const result = await response.json();
+      if (response.ok) {
+        navigate("/");
+        toast.success("Logout Success");
+        dispatch(deleteUserSuccess(result));
+      } else {
+        dispatch(deleteUserFailure(result.msg));
+      }
+    } catch (error) {
+      if (import.meta.env.VITE_ERROR === "development") console.error(error);
+      dispatch(deleteUserFailure("Error failed to fetch API request"));
+    }
+  };
   return (
     <>
       <header className="bg-slate-200 shadow-md">
@@ -16,8 +67,12 @@ function Navbar() {
               type="text"
               placeholder="Search..."
               className="bg-transparent focus:outline-none w-24 sm:w-64"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
-            <FaSearch className="text-slate-600" />
+            <button onClick={handleSubmit}>
+              <FaSearch className="text-slate-600" />
+            </button>
           </form>
           <ul className="flex gap-4 items-center">
             <Link to="/">
@@ -30,17 +85,59 @@ function Navbar() {
                 About
               </li>
             </Link>
-            {!currentUser ? (
-              <Link to="/auth/login">
-                <li className="text-slate-700 hover:underline">Login</li>
-              </Link>
+            {currentUser ? (
+              <div className="relative">
+                <button type="button" onClick={() => setOpen(!open)}>
+                  <img
+                    src={currentUser.avator}
+                    alt="profile"
+                    className="rounded-full h-10 w-10 object-cover"
+                  ></img>
+                </button>
+                {open && (
+                  <ul className="absolute bg-slate-600 text-white font-medium text-sm text-center rounded-md py-2 px-6 cursor-pointer opacity-80 mt-1 top-full right-0 w-44">
+                    <li>
+                      <Link
+                        className="hover:bg-red-800 hover:font-bold hover:px-1 rounded-md"
+                        to="/profile"
+                      >
+                        Profile
+                      </Link>
+                    </li>
+                    <hr />
+                    <li>
+                      <Link
+                        className="hover:bg-red-800 hover:font-bold rounded-md"
+                        to="/create-listing"
+                      >
+                        Create Listing
+                      </Link>
+                    </li>
+                    <hr />
+                    <li>
+                      <Link
+                        className="hover:bg-red-800 hover:font-bold hover:px-1 rounded-md"
+                        to="/userListings"
+                      >
+                        Your Listings
+                      </Link>
+                    </li>
+                    <hr />
+                    <li>
+                      <button
+                        className="hover:bg-red-800 hover:font-bold hover:px-1 rounded-md"
+                        type="button"
+                        onClick={handleLogOut}
+                      >
+                        Log Out
+                      </button>
+                    </li>
+                  </ul>
+                )}
+              </div>
             ) : (
-              <Link to={"/user/settings"}>
-                <img
-                  src={currentUser.avator}
-                  alt="profile"
-                  className="rounded-full h-10 w-10 object-cover"
-                ></img>
+              <Link to="/login">
+                <li className="text-slate-700 hover:underline">Login</li>
               </Link>
             )}
           </ul>
